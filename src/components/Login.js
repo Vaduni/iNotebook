d83ca7f1845b9e729037c06f8cbc0fb027d62a31
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-08-05 12:08:44 +0530

    added sign up link for new users on login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, Link } from "react";
 import { useNavigate } from "react-router-dom";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import Loading from "./Loading";
 import { LogIn } from "lucide-react";
 
@@ -13,13 +13,19 @@ const Login = (props) => {
     e.preventDefault();
     setLoading(true);
     try {
-     const response = await fetch("https://inotebook-backend-kgs1.onrender.com/api/auth/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email: credentials.email, password: credentials.password }),
-      });
+      const response = await fetch(
+        "https://inotebook-backend-kgs1.onrender.com/api/auth/login",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: credentials.email,
+            password: credentials.password,
+          }),
+        }
+      );
 
       const json = await response.json();
       console.log(json);
@@ -46,7 +52,7 @@ const Login = (props) => {
       className="flex-grow bg-cover bg-center flex justify-center items-center px-4"
       style={{
         backgroundImage: "url('/images/clouds.png')",
-        minHeight: "calc(100vh - 32px - 10px)", 
+        minHeight: "calc(100vh - 32px - 10px)",
       }}
     >
       <div className="relative max-w-3xl w-full flex items-center justify-center">
@@ -64,7 +70,12 @@ const Login = (props) => {
           ) : (
             <form onSubmit={handleSubmit} className="space-y-5 sm:space-y-6">
               <div>
-                <label htmlFor="email" className="block text-sm font-medium text-gray-950">Email</label>
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium text-gray-950"
+                >
+                  Email
+                </label>
                 <input
                   type="email"
                   name="email"
@@ -74,10 +85,17 @@ const Login = (props) => {
                   className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-brand-accent focus:outline-none"
                   aria-describedby="emailHelp"
                 />
-                <p className="text-xs text-blue-950 mt-1">We'll never share your email with anyone else.</p>
+                <p className="text-xs text-blue-950 mt-1">
+                  We'll never share your email with anyone else.
+                </p>
               </div>
               <div>
-                <label htmlFor="password" className="block text-sm font-medium text-gray-950">Password</label>
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-medium text-gray-950"
+                >
+                  Password
+                </label>
                 <input
                   type="password"
                   name="password"
@@ -86,6 +104,12 @@ const Login = (props) => {
                   onChange={onChange}
                   className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-brand-accent focus:outline-none"
                 />
+                <p className="mt-2 text-sm text-gray-700">
+                  Don’t have an account?{" "}
+                  <Link to="/Signup" className="text-blue-600 hover:underline">
+                    Sign up
+                  </Link>
+                </p>
               </div>
               <button
                 type="submit"
