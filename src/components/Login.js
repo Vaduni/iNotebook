27e54416447934bf9537421f1627e126a35fa876
Vaduni Niranjan <vaduniniranjan@gmail.com>
2025-08-05 12:14:21 +0530

    typo fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,5 @@
-import React, { useState, Link } from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Loading from "./Loading";
